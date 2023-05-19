07cfa9545a0130e41b917ac971d3f750b8fd8eb2
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2023-05-19 19:55:41 +0800

    integrate ai model

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 import datetime
+
+import tensorflow as tf
+
+from clean_text import clean_texts
+
+model = tf.keras.models.load_model('models/cyberbullying-bdlstm.h5')
+
+with open("models/tokenizer.json") as file:
+    tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(
+        file.read()
+    )
+
+stopwords = [i.lower() for i in nltk.corpus.stopwords.words('english') + [chr(i) for i in range(97, 123)]]
 
 app = Flask(__name__)
 
 convos = []
 
 @app.route("/")
-def home(): return render_template("index.html")
+def home():
+    return render_template("index.html")
 
+@app.route("/cyberbully")
+def cyberbully():
+    messages = clean_texts(request.args.get("msgs"))
+    return jsonify(dict(scores=model.predict(messages).T[0]))
+    
 time = lambda: datetime.datetime.now().strftime("%H:%M")
 
 @app.route("/get")
