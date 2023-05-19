dd74c915c4a00831d1a0150f7eb53f19abc1f5e4
Prannaya <prannayagupta@gmail.com>
2023-05-19 19:57:58 +0800

    move css to static

--- app.py
+++ app.py
@@ -11,12 +11,19 @@ def home(): return render_template("index.html")
 
 time = lambda: datetime.datetime.now().strftime("%H:%M")
 
+@app.route("/getUserHtml")
+def getUser():
+    userText = request.args.get('msg')
+    return {"user": f"<div class='container darker'><p class='user-msg'>{userText}</p><span class='time-right'>{time()}</span></div>"}
+
+
 @app.route("/get")
 def get():
     userText = request.args.get('msg')
     botText = requests.post("https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill", headers={"Authorization": "Bearer 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"}, json={"inputs": {"past_user_inputs": [i[0] for i in convos], "generated_responses": [i[1] for i in convos], "text": userText}, "parameters": {"repetition_penalty": 1.33}}).json()["generated_text"]
     convos.append((userText, botText))
-    return {"user": f"<div class='container darker'><p>{userText}</p><span class='time-right'>{time()}</span></div>", "bot": f"<div class='container'><p>{botText}</p><span class='time-left'>{time()}</span></div>"}
+    return {"user": f"<div class='container darker'><p class='user-msg'>{userText}</p><span class='time-right'>{time()}</span></div>", "bot": f"<div class='container'><p>{botText}</p><span class='time-left'>{time()}</span></div>"}
 
 
-if __name__ == "__main__": app.run()
+if __name__ == "__main__": 
+    app.run(debug=True)
